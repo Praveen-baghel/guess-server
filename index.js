696e817ce6b7e0e4d1a3c1c35f3455f15b8bd7f0
Praveen-baghel <praveenbaghel88990@gmail.com>
2023-09-22 20:21:31 +0530

    second commit

--- index.js
+++ index.js
@@ -195,6 +195,6 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(port, () => {
+server.listen(process.env.PORT || port, () => {
   console.log("Server started and running on port:" + port);
 });
